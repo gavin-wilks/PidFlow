ad97f7b8a9254ea5e56a53c728edd608d92361de
gavin.wilks <gavin.wilks@gmail.com>
2021-10-26 21:38:34 -0400

    More updates for 2019 data and additional histograms

--- RunQA/StRoot/StRunQAMaker/StRunQAMaker.cxx
+++ RunQA/StRoot/StRunQAMaker/StRunQAMaker.cxx
@@ -52,13 +52,14 @@ int StRunQAMaker::Init()
   mRunQAUtility = new StRunQAUtility(mEnergy);
   mRunQAUtility->initRunIndex(); // initialize std::map for run index
   mRunQAProManager = new StRunQAProManager();
-
+  
+  /*
   if(!mRefMultCorr)
   {
     if(!mRunQACut->isBES()) mRefMultCorr = CentralityMaker::instance()->getgRefMultCorr_Run14_AuAu200_VpdMB5_P16id(); // 200GeV_2014
     if(mRunQACut->isBES()) mRefMultCorr = CentralityMaker::instance()->getRefMultCorr(); // BESII
   }
-
+  */
   if(mMode == 0)
   { // QA
     mFile_QA= new TFile(mOutPut_RunQA.c_str(),"RECREATE");
@@ -134,16 +135,18 @@ int StRunQAMaker::Make()
     const unsigned int nTracks = mPicoDst->numberOfTracks(); // get number of tracks
 
     // StRefMultCorr Cut & centrality
+    /*
     if(!mRefMultCorr)
     {
       LOG_WARN << " No mRefMultCorr! Skip! " << endl;
       return kStErr;
     }
-
+    */
+    /*
     mRefMultCorr->init(runId);
     if(!mRunQACut->isBES()) mRefMultCorr->initEvent(grefMult,vz,zdcX); // 200GeV_2014
     if(mRunQACut->isBES()) mRefMultCorr->initEvent(refMult,vz,zdcX); // BES-II might need Luminosity corrections
-
+    */
     /*
     if(mRefMultCorr->isBadRun(runId))
     {
@@ -153,11 +156,11 @@ int StRunQAMaker::Make()
     */
 
     // vz sign
-    int vz_sign = 0; // 0 for -vz || 1 for vz
-    vz > 0.0 ? vz_sign = 1 : vz_sign = 0;
+    //int vz_sign = 0; // 0 for -vz || 1 for vz
+    //vz > 0.0 ? vz_sign = 1 : vz_sign = 0;
 
-    const int cent9 = mRefMultCorr->getCentralityBin9(); // get Centrality9
-    const double reweight = mRefMultCorr->getWeight(); // get Centrality9
+    //const int cent9 = mRefMultCorr->getCentralityBin9(); // get Centrality9
+    //const double reweight = mRefMultCorr->getWeight(); // get Centrality9
     const int runIndex = mRunQAUtility->findRunIndex(runId); // find run index for a specific run
     const int triggerBin = mRunQACut->getTriggerBin(mPicoEvent);
     // cout << "runId = " << runId << ", runIndex = " << runIndex << endl;
@@ -167,24 +170,27 @@ int StRunQAMaker::Make()
       return kStErr;
     }
 
-    bool isPileUpEventStRunQACut = mRunQACut->isPileUpEvent(grefMult,numOfBTofMatch,numOfBTofHits); // 200GeV
-    if(mRunQACut->isBES()) isPileUpEventStRunQACut = mRunQACut->isPileUpEvent(refMult,numOfBTofMatch,numOfBTofHits); // 54 GeV | always return false for 27 GeV
-    bool isPileUpEventStRefMultCorr = !mRefMultCorr->passnTofMatchRefmultCut(1.0*refMult, 1.0*numOfBTofMatch); // 27 GeV | always return !true for other energies
-    bool isPileUpEvent = isPileUpEventStRunQACut || isPileUpEventStRefMultCorr;
+    //bool isPileUpEventStRunQACut = mRunQACut->isPileUpEvent(grefMult,numOfBTofMatch,numOfBTofHits); // 200GeV
+    //if(mRunQACut->isBES()) isPileUpEventStRunQACut = mRunQACut->isPileUpEvent(refMult,numOfBTofMatch,numOfBTofHits); // 54 GeV | always return false for 27 GeV
+    //bool isPileUpEventStRefMultCorr = !mRefMultCorr->passnTofMatchRefmultCut(1.0*refMult, 1.0*numOfBTofMatch); // 27 GeV | always return !true for other energies
+    //bool isPileUpEvent = isPileUpEventStRunQACut || isPileUpEventStRefMultCorr;
     // cout << "isPileUpEvent = " << isPileUpEvent << ", isPileUpEventStRunQACut = " << isPileUpEventStRunQACut << ", isPileUpEventStRefMultCorr = " << isPileUpEventStRefMultCorr << endl;
 
     if(mMode == 0)
     { // fill QA before event cuts
-      mRunQAProManager->fillRunQA_Event(triggerBin,runIndex,refMult,grefMult,zdcX,vx,vy,vz,0);
-      mRunQAHistoManager->fillEventQA_RefMult(triggerBin,refMult,grefMult,cent9,reweight,numOfBTofHits,numOfBTofMatch,0); // wo event cut
+      mRunQAProManager->fillRunQA_Event(triggerBin,runIndex,refMult,grefMult,zdcX,vx,vy,vz,0); 
+      //mRunQAHistoManager->fillEventQA_RefMult(triggerBin,refMult,grefMult,cent9,reweight,numOfBTofHits,numOfBTofMatch,0); // wo event cut
+      mRunQAHistoManager->fillEventQA_RefMult(triggerBin,refMult,grefMult,0,0,numOfBTofHits,numOfBTofMatch,0); // wo event cut
       mRunQAHistoManager->fillEventQA_Vertex(triggerBin,vx,vy,vz,vzVpd,0);
       mRunQAHistoManager->fillEventQA_Trigger(triggerBin,0);
-
-      if(mRunQACut->passEventCut(mPicoDst) && !isPileUpEvent)
+      
+      if(mRunQACut->passEventCut(mPicoDst))
+      //if(mRunQACut->passEventCut(mPicoDst) && !isPileUpEvent)
       { // apply Event Cuts for anlaysis 
 	mRunQAProManager->fillRunQA_Event(triggerBin,runIndex,refMult,grefMult,zdcX,vx,vy,vz,1);
-	mRunQAHistoManager->fillEventQA_RefMult(triggerBin,refMult,grefMult,cent9,reweight,numOfBTofHits,numOfBTofMatch,1); // with event cut
-	mRunQAHistoManager->fillEventQA_Vertex(triggerBin,vx,vy,vz,vzVpd,1);
+	//mRunQAHistoManager->fillEventQA_RefMult(triggerBin,refMult,grefMult,cent9,reweight,numOfBTofHits,numOfBTofMatch,1); // with event cut
+	mRunQAHistoManager->fillEventQA_RefMult(triggerBin,refMult,grefMult,0,0,numOfBTofHits,numOfBTofMatch,1); // with event cut
+        mRunQAHistoManager->fillEventQA_Vertex(triggerBin,vx,vy,vz,vzVpd,1);
 	mRunQAHistoManager->fillEventQA_Trigger(triggerBin,1);
 
 	for(unsigned int i_track = 0; i_track < nTracks; i_track++) // track loop
@@ -195,9 +201,7 @@ int StRunQAMaker::Make()
 	    continue;
 	  }
 
-	  // get pico track info
-	  // TVector3 primMom = picoTrack->pMom();
-	  // TVector3 globMom = picoTrack->gMom();
+	  // get pico track info 
 	  TVector3 primMom;
 	  float primPx    = picoTrack->pMom().x(); // x works for both TVector3 and StThreeVectorF
 	  float primPy    = picoTrack->pMom().y();
@@ -216,19 +220,27 @@ int StRunQAMaker::Make()
 	  int nHitsDEdx    = picoTrack->nHitsDedx();
 	  float dEdx       = picoTrack->dEdx();
 	  short charge     = picoTrack->charge();
-	  float beta       = mRunQACut->getBeta(mPicoDst,i_track);
+          float beta       = mRunQACut->getBeta(mPicoDst,i_track);
 	  float mass2      = mRunQACut->getPrimaryMass2(mPicoDst,i_track);
+          float nSigmaPi   = picoTrack->nSigmaPion();
+          float nSigmaK    = picoTrack->nSigmaKaon();
+          float nSigmaP    = picoTrack->nSigmaProton();
+          float nSigmaE    = picoTrack->nSigmaElectron(); 
 
 	  mRunQAHistoManager->fillTrackQA_Kinematics(triggerBin,primMom,globMom, 0); // wo track cut
 	  mRunQAHistoManager->fillTrackQA_Quliaty(triggerBin,gDCA,nHitsFit,nHitsMax,nHitsDEdx,0);
 	  mRunQAHistoManager->fillTrackQA_PID(triggerBin,primMom.Mag(),charge,dEdx,beta,mass2,0);
-	  mRunQAProManager->fillRunQA_Track(triggerBin,runIndex,gDCA,nHitsFit,primMom,globMom,0);
+          mRunQAHistoManager->fillTrackQA_PID_Additional(triggerBin,nSigmaPi,nSigmaK,nSigmaP,nSigmaE,primMom,charge,mass2,0);
+	  mRunQAProManager->fillRunQA_Track(triggerBin,runIndex,gDCA,nHitsFit,primMom,globMom,dEdx,beta,mass2,nHitsMax,nHitsDEdx,0);
+          mRunQAProManager->fillRunQA_Track_PID(triggerBin,runIndex,nSigmaPi,nSigmaK,nSigmaP,nSigmaE,mass2,0);
 	  if( mRunQACut->passTrackQA(picoTrack,mPicoEvent) ) // apply QA track cut
 	  {
 	    mRunQAHistoManager->fillTrackQA_Kinematics(triggerBin,primMom,globMom, 1); // with track cut
 	    mRunQAHistoManager->fillTrackQA_Quliaty(triggerBin,gDCA,nHitsFit,nHitsMax,nHitsDEdx,1);
 	    mRunQAHistoManager->fillTrackQA_PID(triggerBin,primMom.Mag(),charge,dEdx,beta,mass2,1);
-	    mRunQAProManager->fillRunQA_Track(triggerBin,runIndex,gDCA,nHitsFit,primMom,globMom,1);
+            mRunQAHistoManager->fillTrackQA_PID_Additional(triggerBin,nSigmaPi,nSigmaK,nSigmaP,nSigmaE,primMom,charge,mass2,1);
+	    mRunQAProManager->fillRunQA_Track(triggerBin,runIndex,gDCA,nHitsFit,primMom,globMom,dEdx,beta,mass2,nHitsMax,nHitsDEdx,1);
+            mRunQAProManager->fillRunQA_Track_PID(triggerBin,runIndex,nSigmaPi,nSigmaK,nSigmaP,nSigmaE,mass2,1);
 	  }
 	}
       }
