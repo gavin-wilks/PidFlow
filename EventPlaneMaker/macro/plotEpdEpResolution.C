32c5345722808639dc8d05a5d91bc0374077c6bc
gavin.wilks <gavin.wilks@gmail.com>
2021-11-02 22:47:31 -0400

    Added raw and shifted EPD EP

--- EventPlaneMaker/macro/plotEpdEpResolution.C
+++ EventPlaneMaker/macro/plotEpdEpResolution.C
@@ -26,16 +26,19 @@ double ResolutionFull(double *x_val, double *par)
 void plotEpdEpResolution(int beamEnergy = 1)
 {
 
-  TProfile *p_mResolution[3];
+  TProfile *p_mResolutionRaw[3];
+  TProfile *p_mResolutionShift[3];
   int color[3] = {1,2,4};
 
   //string inputfile = Form("../StRoot/StEventPlaneUtility/Resolution/file_%s_Resolution.root",recoEP::mBeamEnergy[beamEnergy].c_str());
-  string inputfile = Form("../Resolution_file_%s_EpdCorrections_0_1_OUTPUT.root", recoEP::mBeamEnergy[beamEnergy]);
+  string inputfile = Form("../file_%s_EpdResults_0_1.root", recoEP::mBeamEnergy[beamEnergy]);
   TFile *File_InPut = TFile::Open(inputfile.c_str());
  
   for(int o = 1; o <= 3; o++){
-    string HistName = Form("AveCosDeltaPsi%d",o);
-    p_mResolution[o-1] = (TProfile*)File_InPut->Get(HistName.c_str());
+    string HistName = Form("EpdCos%draw",o);
+    p_mResolutionRaw[o-1] = (TProfile*)File_InPut->Get(HistName.c_str());
+    HistName = Form("EpdCos%dshift",o);
+    p_mResolutionShift[o-1] = (TProfile*)File_InPut->Get(HistName.c_str());
   }
 
   TCanvas *c_play = new TCanvas("c_play","c_play",10,10,800,800);
@@ -49,22 +52,48 @@ void plotEpdEpResolution(int beamEnergy = 1)
   leg->SetFillColor(10);
   leg->SetBorderSize(0);
   for(int o = 1; o <= 3; o++) {
-    p_mResolution[o-1]->SetMarkerColor(color[o-1]);
-    p_mResolution[o-1]->SetLineColor(color[o-1]);
+    p_mResolutionRaw[o-1]->SetMarkerColor(color[o-1]);
+    p_mResolutionRaw[o-1]->SetLineColor(color[o-1]);
     if (o == 1) {
-      p_mResolution[o-1]->SetTitle("EP Resolutions");
-      p_mResolution[o-1]->GetYaxis()->SetRangeUser(0,0.1);
+      p_mResolutionRaw[o-1]->SetTitle("EP Resolutions");
+      p_mResolutionRaw[o-1]->GetYaxis()->SetRangeUser(0,0.1);
       //p_mResolution[o-1]->GetXaxis()->SetTitle("");
-      p_mResolution[o-1]->Draw("pE");
+      p_mResolutionRaw[o-1]->Draw("pE");
     }
     else {
-      p_mResolution[o-1]->Draw("pE same");
+      p_mResolutionRaw[o-1]->Draw("pE same");
     } 
-    p_mResolution[o-1]->SetStats(0);
-    leg->AddEntry(p_mResolution[o-1],Form("EP%d",o),"pE");
+    p_mResolutionRaw[o-1]->SetStats(0);
+    leg->AddEntry(p_mResolutionRaw[o-1],Form("EP%d",o),"pE");
   }   
   leg->Draw("same");
 
-  string FigureName = Form("./figures/EpdEpResolution_%s.pdf",recoEP::mBeamEnergy[beamEnergy].c_str());
+  string FigureName = Form("./figures/EpdEpResolutionRaw_%s.pdf",recoEP::mBeamEnergy[beamEnergy].c_str());
   c_play->SaveAs(FigureName.c_str());
+
+  c_play->cd()->Clear();
+
+  TLegend *leg = new TLegend(0.60,0.70,0.85,0.85);
+  leg->SetFillColor(10);
+  leg->SetBorderSize(0);
+  for(int o = 1; o <= 3; o++) {
+    p_mResolutionShift[o-1]->SetMarkerColor(color[o-1]);
+    p_mResolutionShift[o-1]->SetLineColor(color[o-1]);
+    if (o == 1) {
+      p_mResolutionShift[o-1]->SetTitle("EP Resolutions");
+      p_mResolutionShift[o-1]->GetYaxis()->SetRangeUser(0,0.1);
+      //p_mResolution[o-1]->GetXaxis()->SetTitle("");
+      p_mResolutionShift[o-1]->Draw("pE");
+    }
+    else {
+      p_mResolutionShift[o-1]->Draw("pE same");
+    } 
+    p_mResolutionShift[o-1]->SetStats(0);
+    leg->AddEntry(p_mResolutionShift[o-1],Form("EP%d",o),"pE");
+  }   
+  leg->Draw("same");
+
+  string FigureName = Form("./figures/EpdEpResolutionShift_%s.pdf",recoEP::mBeamEnergy[beamEnergy].c_str());
+  c_play->SaveAs(FigureName.c_str()); 
+
 }
